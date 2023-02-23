a302b05f487f50b5329d2374b690bbdd828745c5
indigosaurus <indigosaurus@hotmail.co.uk>
2023-02-23 17:02:22 +0000

    adding in geolocation

--- src/App.js
+++ src/App.js
@@ -13,8 +13,30 @@ const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [filter, setFilter] = useState("All");
-  const [tasks, setTasks] = useState(props.tasks);
-
+  const [tasks, setTasks] = usePersistedState('tasks',[]); //useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+  const [lastInsertedId, setLastInsertedId] = useState('';)
+  //geoFindMe
+  function geoFindMe() {
+    console.log("geoFindMe", lastInsertedID);
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      //mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      console.log(`Latitude: ${latitude}°, Longitude: ${longitude}°`);
+      locateTask(lastInsertedId, {latitude: latitude, longitude: longitude, error: ""});
+      }
+      function error() {
+        console.log('Unable to retrieve your location');
+      }
+      if(!navigator.geolocation) {
+        console.log('Geolocation is not supported by your browser');
+      } else {
+        console.log('Locating...');
+        navigator.geolocation.getCurrentPosition(success, error);
+      }
+  }
+ //toggleTaskCompleted
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
       // if this task has the same ID as the edited task
@@ -28,13 +50,17 @@ function App(props) {
     setTasks(updatedTasks);
     localStorage.setItem('tasks',JSON.stringify(updatedTasks));
   }
+  //deleteTask
   function deleteTask(id) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
     localStorage.setItem('tasks',JSON.stringify(remainingTasks));
   }
+  //editTask
   function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
+    console.log("editTask before");
+    console.log(tasks);
+    const editedTaskList = tasks.map(task => {
       // if this task has the same ID as the edited task
       if (id === task.id) {
         //
@@ -45,7 +71,21 @@ function App(props) {
     setTasks(editedTaskList); 
     localStorage.setItem('tasks',JSON.stringify(editedTaskList));
   }
-
+  //locateTask
+  function locateTask(id, location) {
+    console.log("locate Task", id, " before");
+    console.log(location, tasks);
+    const locatedTaskList = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        return {...task, location: location}
+      }
+      return task;
+    });
+  console.log(locatedTaskList);
+  setTasks(locatedTaskList);
+  }
+ //TODO
   const taskList = tasks
     .filter(FILTER_MAP[filter])
     .map((task) => (
@@ -54,12 +94,14 @@ function App(props) {
         name={task.name}
         completed={task.completed}
         key={task.id}
+        latitude={task.location.latitude}
+        longitude={task.location.longitude}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
         editTask={editTask}
       />
     ));
-
+  //filter 
   const filterList = FILTER_NAMES.map((name) => (
     <FilterButton
       key={name}
@@ -68,22 +110,27 @@ function App(props) {
       setFilter={setFilter}
     />
   ));
-
+ //addTask
   function addTask(name) {
-    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    const id = "todo-" + nanoid();
+    const newTask = { id: id, name: name, completed: false, location: {latitude:"##", longitude:"##", error:"##"}};
+    setLastInsertedId(id);
     setTasks([...tasks, newTask]);
-    localStorage.setItem('tasks',JSON.stringify(newTask));
+    // const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    // setTasks([...tasks, newTask]);
+    // localStorage.setItem('tasks',JSON.stringify(newTask));
   }
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-
+ //html to return
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form addTask={addTask} />
-
-      <div className="filters btn-group stack-exception">{filterList}</div>
-      <h2 id="list-heading">{headingText}</h2>
+      <Form addTask={addTask} geoFindMe={geoFindMe}/>
+      <div className="filters btn-group stack-exception">
+        {filterList}
+        </div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
