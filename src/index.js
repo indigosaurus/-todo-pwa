d121bb97b53f822bd96f5a845313e7d9ef40e0ce
indigosaurus <indigosaurus@hotmail.co.uk>
2023-02-23 13:48:30 +0000

    updating empty react PWA with app coding from src.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,27 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+*/
+
+//localStorage.setItem('tasks',JSON.stringify(DATA));
+
+const DATA = JSON.parse(localStorage.getItem('tasks')) || [];
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//     <App tasks={DATA} />
+//   </React.StrictMode>,
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
